8fb00758428e2f4f3ba027aad2a9b810ac39e8cd
Bryan Vera <brayanvera93@outlook.com>
2020-10-30 00:10:19 -0500

    Inserción inicial en el repositorio del módulo TareaS3Git

---
+++ TareaS3Git/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TareaS3Git
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_ventana_dos_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+            string user = txtuser.Text;
+            string pass = txtpass.Text;
+
+            if (txtuser.Text == "estudiante2020" && txtpass.Text == "uisrael2020") {
+                await DisplayAlert("Bienvenido", "Datos correctos", "Aceptar");
+            }
+
+
+            else {
+                await DisplayAlert("ERROR", "Datos incorrectos", "Aceptar");
+                return;
+            }
+
+          
+
+
+
+            await Navigation.PushAsync(new Venatana_dos(user, pass));
+        }
+    }
+}
---
+++ TareaS3Git/Venatana_dos.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TareaS3Git
+{
+    public partial class Venatana_dos : ContentPage
+    {
+        public Venatana_dos(string user, string pass)
+        {
+            InitializeComponent();
+
+           lbluser.Text = user;
+        }
+
+        void btnSumar_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+            //try catch
+            try
+            {
+                double seguimiento1 = Convert.ToDouble(txtSeg1.Text);
+                double examen1 = Convert.ToDouble(txtExa1.Text);
+
+                //Resultado
+
+                double parcial1 = (seguimiento1 * 0.3) + (examen1 * 0.2);
+                txtParcial1.Text = parcial1.ToString();
+
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Mensaje de alerta", "ERROR" + ex.Message, "Ok");
+
+            }
+        }
+
+        void btnSumar2_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+            //try catch
+            try
+            {
+                double seguimiento2 = Convert.ToDouble(txtSeg2.Text);
+                double examen2 = Convert.ToDouble(txtExa2.Text);
+
+                //Resultado
+
+                double parcial2 = (seguimiento2 * 0.3) + (examen2 * 0.2);
+                txtParcial2.Text = parcial2.ToString();
+
+
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Mensaje de alerta", "ERROR" + ex.Message, "Ok");
+
+            }
+        }
+
+        void btnNotafin_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+            //try catch
+            try
+            {
+
+
+                //Resultado
+
+                double parcial1 = Convert.ToDouble(txtParcial1.Text);
+                double parcial2 = Convert.ToDouble(txtParcial2.Text);
+
+
+                double notaFinal = parcial1 + parcial2;
+                txtNotafin.Text = notaFinal.ToString();
+
+
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Mensaje de alerta", "ERROR" + ex.Message, "Ok");
+
+            }
+        }
+    }
+}
